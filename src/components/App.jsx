2e5f3ff6b0183e83122d9fdce82b2e1501841c52
tkrupesh14 <info.tkrupesh14@gmail.com>
2022-04-30 13:17:42 +0530

    Alan Added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   VStack,
@@ -8,12 +8,68 @@ import {
   useColorMode,
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import axios from "axios";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [sourceCode, setSourceCode] = useState ("");
+  const [destCode, setDestCode] = useState ("");
+  const [translatedText, setTranslatedText] = useState("");
+
+
   const { colorMode, toggleColorMode } = useColorMode();
   const handleToggleColor = () => {
     toggleColorMode();
   };
+
+ 
+
+  useEffect(() => {
+    alanBtn({
+        key: "f24e93fce78f9884980b39e72a128c432e956eca572e1d8b807a3e2338fdd0dc/stage",
+        onCommand: (commandData) => {
+         console.log(commandData);
+         if(commandData.command === "input"){
+          setInput(commandData.data)
+         }
+         if(commandData.command === "source"){
+          if(commandData.data = "English"){
+            setSourceCode("en")
+          }
+
+         }
+         if(commandData.command === "destination"){
+            if(commandData.data = "English"){
+            setDestCode("en")
+            }
+
+           else if(commandData.data = "Hindi"){
+              setDestCode("hi")
+            }
+             else if(commandData.data = "Aribic"){
+              setDestCode("ar")
+            }
+          }
+        }
+    });
+  }, []);
+
+  useEffect(() => {
+    translate()
+  }, [input, sourceCode, destCode]);
+
+   const translate = () => {
+      let body = {
+        q: input,
+        source: sourceCode,
+        target: destCode
+      }
+      axios.post(`https://libretranslate.de/translate`, body)
+      .then((response) => {
+        setTranslatedText(response.data.translatedText)
+      })
+  }
 
   return (
     <Box textAlign="center" fontSize="xl">
@@ -23,6 +79,7 @@ const App = () => {
           <Button onClick={handleToggleColor}>
             {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
           </Button>
+          <h3>{translatedText}</h3>
         </VStack>
       </Grid>
     </Box>
